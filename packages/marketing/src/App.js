36f0c0d3138bfab80e912f8406800b879687338a
faultyagatha <valeria.barvinska@gmail.com>
2021-01-10 11:52:40 +0100

    add console.log to force the action

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -5,6 +5,8 @@ import { StylesProvider } from '@material-ui/core/styles';
 import Landing from './components/Landing';
 import Pricing from './components/Pricing';
 
+console.log('running marketing service');
+
 export default () => {
   return (
     <div>
