40aa8abfd9362d32d939aaea8a7a0dd267384f79
faultyagatha <valeria.barvinska@gmail.com>
2021-01-20 15:32:54 +0100

    add navigation solution for both isolation and production

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -1,14 +1,32 @@
 import React, { useRef, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 
 import { mount } from 'marketing/MarketingApp';
 
 /** generic approach to decouple our services */
 export default () => {
   const ref = useRef(null);
+  const history = useHistory();
 
+  /** onNavigate: 
+   * a cb fired when we navigate 
+   * in Marketing component*/
   useEffect(() => {
-    mount(ref.current);
-  });
+    const { onParentNavigate } = mount(ref.current, {
+      //nextPathname comes from 
+      //history.location in Marketing App
+      onNavigate: ({ pathname: nextPathname }) => {
+        //pathname comes from this App
+        const { pathname } = history.location;
+        if (pathname !== nextPathname) {
+          history.push(nextPathname);
+        }
+      }
+    });
+
+    //communicate navigation down to Marketing
+    history.listen(onParentNavigate);
+  }, []);
 
   return <div ref={ref} />;
 };
--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,15 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createMemoryHistory } from 'history';
+import { createMemoryHistory, createBrowserHistory } from 'history';
 
 import App from './App';
 
 // Mount function to start up the app
-const mount = (el) => {
+const mount = (el, { onNavigate, defaultHistory }) => {
   //create a copy of history object that
   //will be used for our Router
-  const history = createMemoryHistory();
+  //if we're in isolation, use default history
+  const history = defaultHistory || createMemoryHistory();
+  //listen for navigation events 
+  if (onNavigate) history.listen(onNavigate);
   ReactDOM.render(<App history={history} />, el);
+
+  //communicate navigation from Container
+  //down to Marketing App
+  return {
+    onParentNavigate({ pathname: nextPathname }) {
+      console.log('container just navigated');
+      const { pathname } = history.location;
+      if (pathname !== nextPathname) {
+        history.push(nextPathname);
+      }
+    }
+  }
 };
 
 // If we are in development and in isolation,
@@ -18,7 +33,8 @@ if (process.env.NODE_ENV === 'development') {
   const devRoot = document.querySelector('#_marketing-dev-root');
 
   if (devRoot) {
-    mount(devRoot);
+    //provide a browser history to use in isolation
+    mount(devRoot, { defaultHistory: createBrowserHistory() });
   }
 }
 
