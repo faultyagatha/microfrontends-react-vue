d03f040cd4cc07341d8a1185a6ea2122aa140255
faultyagatha <valeria.barvinska@gmail.com>
2021-01-20 14:52:32 +0100

    add memory history object to marketing component

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
+
 
 import MarketingApp from './components/MarketingApp';
 import Header from './components/Header';
 
+/** prevent scc name collision */
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'co'
+});
+
 export default () => {
   return (
     <Router>
-      <div>
-        <Header />
-        <MarketingApp />
-      </div>
+      <StylesProvider generateClassName={generateClassName}>
+        <div>
+          <Header />
+          <MarketingApp />
+        </div>
+      </StylesProvider>
     </Router>
   );
 };
--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createMemoryHistory } from 'history';
+
 import App from './App';
 
 // Mount function to start up the app
 const mount = (el) => {
-  ReactDOM.render(<App />, el);
+  //create a copy of history object that
+  //will be used for our Router
+  const history = createMemoryHistory();
+  ReactDOM.render(<App history={history} />, el);
 };
 
 // If we are in development and in isolation,
