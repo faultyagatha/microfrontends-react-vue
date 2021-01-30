79d9c44e7d7882aeecf1cf5a6f8dbdc932ca5cec
faultyagatha <valeria.barvinska@gmail.com>
2021-01-30 14:04:01 +0100

    add lazy loading and progress bar component

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
 
-import AuthApp from './components/AuthApp';
-import MarketingApp from './components/MarketingApp';
+import ProgressBar from './components/ProgressBar';
 import Header from './components/Header';
+
+/** lazy loading */
+const MarketingLazy = lazy(() => import('./components/MarketingApp'));
+const AuthLazy = lazy(() => import('./components/AuthApp'));
 
 /** prevent scc name collision */
 const generateClassName = createGenerateClassName({
@@ -17,10 +20,12 @@ export default () => {
       <StylesProvider generateClassName={generateClassName}>
         <div>
           <Header />
-          <Switch>
-            <Route path="/auth" component={AuthApp} />
-            <Route path="/" component={MarketingApp} />
-          </Switch>
+          <Suspense fallback={<ProgressBar />}>
+            <Switch>
+              <Route path="/auth" component={AuthLazy} />
+              <Route path="/" component={MarketingLazy} />
+            </Switch>
+          </Suspense>
         </div>
       </StylesProvider>
     </Router>
